26bd9b4abcfe839a35c364f686c314280b78f4d9
Gerson Janhuel <gersonjanhuel@gmail.com>
2022-06-29 00:32:23 +0700

    Initial Commit

---
+++ CloudKit-HandsOn-Starter/CloudKit_HandsOn_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudKit_HandsOn_StarterApp.swift
+//  CloudKit-HandsOn-Starter
+//
+//  Created by Gerson Janhuel on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudKit_HandsOn_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
